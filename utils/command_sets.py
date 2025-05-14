50d49422268144b5fcd577f49f1fd86c329361f7
galencky <galen147258369@gmail.com>
2025-05-14 16:50:13 +0800

    command set mod

--- utils/command_sets.py
+++ utils/command_sets.py
@@ -1,8 +1,16 @@
-# ── Command word sets ──────────────────────────────────────────────
+"""
+Centralised lists of recognised command words.
+Edit here if you want to add synonyms.
+"""
 
+# conversation control
 new_commands       = {"new", "開始"}
-edu_commands       = {"ed", "education", "衛教"}          # ⇦ NEW
-chat_commands      = {"chat", "聊天"}                     # ⇦ NEW
+
+# mode selection *after* new
+edu_commands       = {"ed", "education", "衛教"}
+chat_commands      = {"chat", "聊天"}
+
+# education-branch commands
 modify_commands    = {"modify", "修改"}
 translate_commands = {"translate", "翻譯", "trans"}
 mail_commands      = {"mail", "寄送"}
